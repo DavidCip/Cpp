76851fd000af1f89aecebee96e1196deab468161
DavidCip <david.ciprian.murar@gmail.com>
2024-11-28 16:59:30 +0200

    Inmultire simpla

---
+++ Inmultire simpla.cpp
@@ -0,0 +1,14 @@
+// Inmultire simpla.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int a, b, c;
+    a = 5, b = 3;
+    c = a * b;
+    std::cout << c;
+}
+
+
