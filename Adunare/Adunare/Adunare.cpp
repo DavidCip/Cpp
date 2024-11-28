3cefe65fca156bbb74f78fff8eeb4b5f9528be47
DavidCip <david.ciprian.murar@gmail.com>
2024-11-28 16:32:40 +0200

    Adunare

---
+++ Adunare/Adunare/Adunare.cpp
@@ -0,0 +1,14 @@
+// Adunare.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int a, b;
+    a = 5;
+    b = 7;
+    std::cout << a + b;
+}
+
+
