4372a0dac441666871c99c3bed4c513091eb246e
DavidCip <david.ciprian.murar@gmail.com>
2024-11-30 12:37:02 +0200

    Tabel inmultire

---
+++ Tabelul inmultire/Tabelul inmultire/Tabelul inmultire.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numar, inmultire;
+    cout << "Introduceti numarul" << endl;
+    cin >> numar;
+
+    for (inmultire=1;inmultire<=10;inmultire++) 
+        {   
+
+        cout << numar << " * " << inmultire << " = " << (numar * inmultire) << endl;
+        
+        }
+
+
+}
+
+
